5ef9402cacca2a78812656732a9dd85ac392ae11
Pratik Aswani <pratikaswani9333@gmail.com>
2022-07-30 13:03:03 +0530

    removed unnecessary code from search

--- search-task/src/components/search/index.js
+++ search-task/src/components/search/index.js
@@ -2,28 +2,12 @@ import React from "react";
 
 import "./styles.modules.css";
 
-const Search = (props) => {
-<<<<<<< HEAD
-  return (
-    <div className="search-box">
-      <input
-        onChange={props.onChange}
-=======
-  //
-
-  const handleText = (e) => {
-    if (e.keyCode === 8) props.onKeyDown(true);
-    props.setQuery(e.target.value);
-    props.setPaginate(1);
-  };
-
+const Search = ({ onChange }) => {
   //
   return (
     <div className="search-box">
       <input
-        onChange={handleText}
-        onKeyDown={handleText}
->>>>>>> de557eb573a1e03f50f82e8813ef74f6b8d0c877
+        onChange={onChange}
         type="text"
         placeholder="Search Characters..."
         id="text"
